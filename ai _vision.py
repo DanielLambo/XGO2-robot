256a3ada83cf84ae2e61ee043a3b3400f783cd06
DanielLambo <146047658+DanielLambo@users.noreply.github.com>
2024-11-23 18:07:24 -0600

    Create ai _vision.py

---
+++ ai _vision.py
@@ -0,0 +1,16 @@
+from xgolib import XGO
+from xgoedu import XGOEDU
+XGO_mini = XGO("xgomini")
+XGO_edu = XGOEDU()
+
+XGO_edu.agesex()
+
+XGO_edu.gestureRecognition()
+
+XGO_edu.face_detect()
+
+XGO_edu.CircleRecognition()
+
+XGO_edu.QRRecognition()
+
+XGO_edu.SpeechRecognition()
